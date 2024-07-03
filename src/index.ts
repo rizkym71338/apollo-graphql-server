4478721ce668f0e71628e407f3268e72e1d68bb4
Rizky Maulana <rizkym71338@gmail.com>
2024-07-03 09:14:47 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
-import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
 
 import { typeDefs, resolvers } from './schema'
 
 const startServer = async () => {
-  const server = new ApolloServer({ typeDefs, resolvers, plugins: [ApolloServerPluginLandingPageDisabled()] })
+  const server = new ApolloServer({ typeDefs, resolvers })
   const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
   console.log(`🚀  Server ready at ${url}`)
 }
