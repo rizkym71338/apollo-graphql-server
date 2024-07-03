57a7c9224fd1356fe82fc1996c02ca7b35632cbc
Rizky Maulana <rizkym71338@gmail.com>
2024-07-03 10:07:27 +0700

    add related data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+// import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 
 import { typeDefs, resolvers } from './schema'
 
 const startServer = async () => {
-  const server = new ApolloServer({ typeDefs, resolvers, introspection: true, plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })] })
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // introspection: true,
+    // plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  })
   const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
   console.log(`🚀  Server ready at ${url}`)
 }
